b797ee21660bc167c303f6e6d0776b30d7413932
2xroygbiv7 <patrick@theonevehicle.com>
2020-07-31 21:51:30 +1000

    Create Color model, ProductColor join table, update Product views, Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
-product_list = [
-  ["BL207","EnerGel Metallic"],
-  ["BK130",".e-ball"],
-  ["N50","Pentel Pen"],
-  ["MWL5"],["Maxiflo Medium Point"],
-  ["PD105T","Techniclick"]
-]
+colors = Color.create([
+  { code: "a", name: "black" },
+  { code: "b", name: "red" },
+  { code: "c", name: "blue" },
+  { code: "d", name: "green" },
+  { code: "v", name: "violet" }
+])
+  
+puts "#{Color.all.length} colors created"
 
-product_list.each do |code, name|
-  Product.create!(
-    code: code,
-    name: name
-  )
-end
+Product.create([
+  { code: "BL207",  name: "EnerGel Metallic",       color_ids: [colors[0].id] },
+  { code: "BK130",  name: ".e-ball",                color_ids: [colors[1].id] },
+  { code: "N50",    name: "Pentel Pen",             color_ids: [colors[2].id] },
+  { code: "MWL5",   name: "Maxiflo Medium Point",   color_ids: [colors[3].id] },
+  { code: "PD105T", name: "Techniclick",            color_ids: [colors[4].id] }
+])
 
 puts "#{Product.all.length} products created"
