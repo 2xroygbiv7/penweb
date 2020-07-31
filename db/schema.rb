a5b26bc0a774124b01012b46a4f875b5a5d5c5b9
2xroygbiv7 <patrick@theonevehicle.com>
2020-07-31 14:42:46 +1000

    Create pages controller, home view, added tailwindcss

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_120702) do
+ActiveRecord::Schema.define(version: 2020_07_31_043859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
