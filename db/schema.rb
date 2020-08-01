fd6bab50431ce0652e5400365f2b7cc7a2128474
2xroygbiv7 <patrick@theonevehicle.com>
2020-08-02 00:38:45 +1000

    Added Category model, updated views & seeds for Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_064954) do
+ActiveRecord::Schema.define(version: 2020_08_01_111546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "colors", force: :cascade do |t|
     t.string "code"
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
   end
 
   create_table "product_colors", force: :cascade do |t|
@@ -38,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_07_31_064954) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
   add_foreign_key "product_colors", "colors"
   add_foreign_key "product_colors", "products"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,25 @@ colors = Color.create([
   
 puts "#{Color.all.length} colors created"
 
+categories = Category.create([
+  { name: "energel" },
+  { name: "ballpoint" },
+  { name: "permanent marker" },
+  { name: "mechanical pencil" },
+  { name: "brush pen" },
+  { name: "whiteboard marker" },
+  { name: "gift collection" }
+])
+  
+puts "#{Category.all.length} colors created"
+
 Product.create([
-  { code: "BL207",  name: "EnerGel Metallic",       color_ids: [colors[0].id] },
-  { code: "BK130",  name: ".e-ball",                color_ids: [colors[1].id] },
-  { code: "N50",    name: "Pentel Pen",             color_ids: [colors[2].id] },
-  { code: "MWL5",   name: "Maxiflo Medium Point",   color_ids: [colors[3].id] },
-  { code: "PD105T", name: "Techniclick",            color_ids: [colors[4].id] }
+  { code: "BL407",  name: "EnerGel Metallic",     color_ids: [colors[0].id, colors[1].id], category_ids: [categories[categories.index{|cat| cat[:name] == "energel"}].id, categories[categories.index{|cat| cat[:name] == "gift collection"}].id] },
+  { code: "BK130",  name: ".e-ball",              color_ids: [colors[1].id, colors[2].id], category_ids: [categories[categories.index{|cat| cat[:name] == "ballpoint"}].id] },
+  { code: "N50",    name: "Pentel Pen",           color_ids: [colors[2].id, colors[3].id], category_ids: [categories[categories.index{|cat| cat[:name] == "permanent marker"}].id] },
+  { code: "MWL5",   name: "Maxiflo Medium Point", color_ids: [colors[3].id, colors[4].id], category_ids: [categories[categories.index{|cat| cat[:name] == "whiteboard marker"}].id] },
+  { code: "PD105T", name: "Techniclick",          color_ids: [colors[4].id, colors[0].id], category_ids: [categories[categories.index{|cat| cat[:name] == "mechanical pencil"}].id] },
+  { code: "GFKP3", name: "Pocket Brush",          color_ids: [colors[1].id],               category_ids: [categories[categories.index{|cat| cat[:name] == "brush pen"}].id] }
 ])
 
 puts "#{Product.all.length} products created"
